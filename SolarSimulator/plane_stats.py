842f39e33b0cf42ced04603e66965403e84ab251
bepstein8 <bepstein8@gatech.edu>
2024-05-01 12:19:55 -0400

    plot of collected solar power

--- SolarSimulator/plane_stats.py
+++ SolarSimulator/plane_stats.py
@@ -7,14 +7,20 @@ from Seaplane_base import Seaplane
 # Define constant parameters
 lat = 29.02291491363789
 lon = -90.23223029442693
-tz = 'Etc/GMT+5'
+tz = 'Etc/GMT+6'
 pdc0 = 80
 gamma = -0.0043
 
-plane = Seaplane(lat, lon, tz, pdc0,gamma,cd0=0.0145,cdtot = 0.025,n_tot=.75,S=0.38,weight=4*9.81,voltage=37.0,capacity=10)
+plane_t = Seaplane(lat, lon, tz, pdc0,gamma,cd0=0.0145,tracking=True,cdtot = 0.025,n_tot=.75,S=0.38,weight=4*9.81,voltage=37.0,capacity=10)
+plane_f = Seaplane(lat, lon, tz, pdc0,gamma,cd0=0.0145,tracking=False,cdtot = 0.025,n_tot=.75,S=0.38,weight=4*9.81,voltage=37.0,capacity=10)
 
-print(plane.calc_collected_energy((2021,2021),(1,1),(1,3),(1,12)))
+times, P_solar_f = plane_f.calc_collected_energy((2019,2019),(1,1),(1,30))
+times, P_solar_t = plane_t.calc_collected_energy((2019,2019),(1,1),(1,30))
 
+# DC Power Absorbed Plot
+fig = plt.plot(P_solar_f)
+fig = plt.plot(P_solar_t)
+plt.show()
 
 # # Endurance and P_req calculations
 # E = []

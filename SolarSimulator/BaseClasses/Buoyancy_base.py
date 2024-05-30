9a8e82e57a0a2cff3cce8adb561ad1d704abb576
bepstein8 <bepstein8@gatech.edu>
2024-05-30 14:07:28 -0400

    Create buoyancy class

---
+++ SolarSimulator/BaseClasses/Buoyancy_base.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+from Tools import stl_slice
+from tqdm import tqdm
+
+
+class Buoyancy:
+    """Class representing a seaplane"""
+    def __init__(self):
+        pass
+    
+    def is_stable(self):
+        pass

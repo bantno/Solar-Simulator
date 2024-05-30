4b08f5d7172a2508c0c52283d219e99dfdca0ce9
bepstein8 <bepstein8@gatech.edu>
2024-05-30 14:07:38 -0400

    Remove unused classes

--- SolarSimulator/BaseClasses/photovoltaic_base.py
+++
@@ -1,6 +0,0 @@
-"""Module containing base class for solar panels"""
-
-class PhotovoltaicBase:
-    """Base class for solar panels"""
-    def __init__(self):
-        pass
--- SolarSimulator/BaseClasses/vehicle_base.py
+++
@@ -1,7 +0,0 @@
-"""Module containg base class for vehicles"""
-
-class VehicleBase:
-    """Base class for vehicles"""
-    def __init__(self, id: any, lon: float, lat: float):
-        """Constructor method that sets the basic parameters for a vehicle"""
-        pass

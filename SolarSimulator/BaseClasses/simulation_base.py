9688807e3f66ee495b7171811ddc3811a280c661
Brian Epstein <bepstein8@gatech.edu>
2024-12-16 09:16:16 -0500

    Correct data path

--- SolarSimulator/BaseClasses/simulation_base.py
+++ SolarSimulator/BaseClasses/simulation_base.py
@@ -163,7 +163,7 @@ class Simulation:
         # Simulate the behavior
         if algo == "Threshold":
             for i in tqdm(range(num_runs), desc=f"{algo} Simulation", leave=False, mininterval=1):
-                actual_data = self._load_weather_data(dt,directory=r"Data\SYNTHETIC_DATA",i=i)
+                actual_data = self._load_weather_data(dt,directory=r"Data\SYNTHETIC_DATA\lat30",i=i)
                 actual_data = actual_data.loc[start_date:end_date]
                 auto.data=actual_data
                 if self.save_history:

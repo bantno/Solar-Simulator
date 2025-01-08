70250d67dc4b5be18258ad6abd0906ddfb52f679
Brian Epstein <bepstein8@gatech.edu>
2025-01-08 16:55:59 -0500

    Initial commit

---
+++ SolarSimulator/Scripts/plot_all _results.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from BaseClasses.plotting_base import DataProcessor
+
+dire = r"Results\Analysis"
+processor = DataProcessor(directory=dire)  # Use "." for the current directory
+processor.process_files()
+df = processor.get_results_df()
+processor.plot_all_data(dire)

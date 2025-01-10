1d756f5ef980a1989f98e279907280fed7e872f7
Brian Epstein <bepstein8@gatech.edu>
2025-01-10 15:21:07 -0500

    renamed

--- SolarSimulator/Scripts/plot_all_results.py
+++ SolarSimulator/Scripts/plot_all_results.py
@@ -3,11 +3,11 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from BaseClasses.plotting_base import DataProcessor
 
-dire = r"Results\Analysis-full"
+dire = r"Results\Analysis"
 processor = DataProcessor(directory=dire)  # Use "." for the current directory
 processor.process_files()
 df = processor.get_results_df()
 processor.plot_all_data(dire)
 # processor.plot_optimal_battery_capacity(df,dire)
-processor.plot_combined_data(dire)
+# processor.plot_combined_data(dire)
 

e8671a07ab53676df3128e03b2b5655b6d01fe99
Brian Epstein <bepstein8@gatech.edu>
2025-01-13 23:17:32 -0500

    state history plot

--- SolarSimulator/Scripts/plot_state_history.py
+++ SolarSimulator/Scripts/plot_state_history.py
@@ -8,5 +8,5 @@ from BaseClasses.plotting_base import StateHistoryPlotter
 direct = r"Results\Analysis"
 utc_offset = timezone(timedelta(hours=0))
 start_date = pd.to_datetime(datetime(2024,1,1).replace(tzinfo=utc_offset))
-solar = StateHistoryPlotter(direct,start_date,"60min")
+solar = StateHistoryPlotter(direct,start_date,"10min")
 solar.plot_data(direct)

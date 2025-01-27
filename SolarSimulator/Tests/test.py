2bc680eaeafa4aa8996c0dac9a057f697a90da3e
Brian Epstein <bepstein8@gatech.edu>
2025-01-27 18:50:20 -0500

    test new_mdp

--- SolarSimulator/Tests/test.py
+++ SolarSimulator/Tests/test.py
@@ -1,32 +1,56 @@
-import pandas as pd
+import os
+import sys
 import numpy as np
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+from BaseClasses.new_mdp import ExpectedValueTable  
+from BaseClasses.seaplane_base import Seaplane
 
-def get_whale_observation_probabilities(time_index: pd.DatetimeIndex):
-    # Define the time intervals and probabilities
-    time_intervals = [
-        ("0600", "0800", 0.082),
-        ("0800", "1000", 0.098),
-        ("1000", "1200", 0.095),
-        ("1200", "1400", 0.217),
-        ("1400", "1600", 0.215),
-        ("1600", "2000", 0.278)
-    ]
-    
-    # Function to get the whale observation probability for a given time
-    def get_probability_for_time(time):
-        time_str = time.strftime("%H%M")
-        for interval_start, interval_end, prob in time_intervals:
-            if interval_start <= time_str < interval_end:
-                return prob
-        return 0.0  # Return 0.0 if the time doesn't fall into any interval
+lat=25
+lon=-90
+tz="Etc/GMT-5"
+pdc0=0
+gamma=-0.0047
+capacity_ah=50.0
+voltage=22.2
+Cdtot=0.0
+Cd0=0.02584
+S=0.653
+af_mass=8.8
+cruise_speed=20.0
+rho=1.19
+N_PROP=0.82
+N_ESC=0.9
+n_tot = N_ESC*N_PROP
+start_date="2019-07-01"
+end_date="2019-08-02"
+dt=30
+num_runs=10000
+visualize=False
+save_dir="."
+show=False
+use_expected=False
 
-    # Apply the function to the DatetimeIndex and return the probabilities
-    probabilities = np.array(time_index.map(get_probability_for_time))
-    return probabilities
+plane = Seaplane(
+            lat=30, lon=-90, tz=0, pdc0=0, gamma=0,
+            cd0=Cd0, cs=True, tracking=False, cdtot=Cdtot,
+            n_tot=n_tot, S=S, af_mass=af_mass,
+            voltage=voltage, capacity=capacity_ah
+        )
 
-# Example usage with a sample DatetimeIndex
-time_index = pd.date_range("2024-01-01 00:00", "2024-06-01 23:45", freq="15min", tz="UTC-06:00")
-probabilities = get_whale_observation_probabilities(time_index)
+expected_solar_data = np.zeros((int(25/0.5),3))
+expected_solar_data[:,0] = 1
+expected_solar_data[:,1] = 1000
 
-# Displaying the first few probabilities
-print(probabilities)
+expected_wind_data = np.zeros((int(25/0.5),3))
+expected_wind_data[:,0] = 5. # loc
+expected_wind_data[:,1] = 20. # scale
+
+whale_observation_data = np.zeros((int(25/0.5),1))
+whale_observation_data[12:24] = 1.0
+mdp_model = ExpectedValueTable(plane,
+                               expected_solar_data,
+                               expected_wind_data,
+                               whale_observation_data,
+                               soc_increment=1,
+                               timestep_min=dt)
+mdp_model.generate_ev_table()

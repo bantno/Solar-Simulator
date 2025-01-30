e68b1c4bfdf25f4a3f291c9d2c84ab6a0041dfb9
Brian Epstein <brian.antnoepstein@live.com>
2025-01-29 20:29:30 -0500

    switch start date to 2025

--- SolarSimulator/Scripts/plot_state_history.py
+++ SolarSimulator/Scripts/plot_state_history.py
@@ -5,8 +5,8 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from BaseClasses.plotting_base import StateHistoryPlotter
 
-direct = r"Results\Analysis"
-utc_offset = timezone(timedelta(hours=0))
-start_date = pd.to_datetime(datetime(2024,1,1).replace(tzinfo=utc_offset))
+direct = r"Results\Analysis2weeks\state_history"
+utc_offset = timezone(timedelta(hours=-6))
+start_date = pd.to_datetime(datetime(2025,1,1).replace(tzinfo=utc_offset))
 solar = StateHistoryPlotter(direct,start_date,"15min")
 solar.plot_data(direct)

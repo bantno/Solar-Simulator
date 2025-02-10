69a17865fdfad31881733b3843e80508ccb2e8e5
Brian Epstein <bepstein8@gatech.edu>
2025-02-10 14:23:41 -0500

    formatting

--- SolarSimulator/Tools/plot_ev_table.py
+++ SolarSimulator/Tools/plot_ev_table.py
@@ -1,8 +1,19 @@
-import numpy as np
+""" A script to plot the expected value table for the wind and solar simulations. """
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_data(x, y, plot_type='line', xlabel=None, ylabel=None, title=None, legend=None, style=None):
+
+def plot_data(
+    x,
+    y,
+    plot_type="line",
+    xlabel=None,
+    ylabel=None,
+    title=None,
+    legend=None,
+    style=None,
+):
     """
     A generic plotting function to create different types of plots.
 
@@ -19,14 +30,14 @@ def plot_data(x, y, plot_type='line', xlabel=None, ylabel=None, title=None, lege
     # plt.figure(figsize=(8, 4))
     if style:
         plt.style.use(style)
-    
-    if plot_type == 'line':
+
+    if plot_type == "line":
         plt.plot(x, y, label=legend)
-    elif plot_type == 'scatter':
+    elif plot_type == "scatter":
         plt.scatter(x, y, label=legend)
-    elif plot_type == 'bar':
+    elif plot_type == "bar":
         plt.bar(x, y, label=legend)
-    elif plot_type == 'hist':
+    elif plot_type == "hist":
         plt.hist(y, bins=30, label=legend)
     else:
         print(f"Unsupported plot type: {plot_type}")
@@ -42,9 +53,17 @@ def plot_data(x, y, plot_type='line', xlabel=None, ylabel=None, title=None, lege
         plt.legend()
     plt.grid(True)
 
-    
 
-table = pd.read_csv(r"Data\TEST_CASES\Wind\Meeting-Results\varyWind-varyWhale\ev_table.csv", header=None).to_numpy()
-plot_data(range(len(table[100,:])),table[100,:],legend="Moored")
-plot_data(range(len(table[100,:])),table[201,:],xlabel="Stage",ylabel="Expected Value",title="Flying vs Moored for Battery Capacity = 100%",legend="Flying")
+table = pd.read_csv(
+    r"Data\TEST_CASES\Wind\Meeting-Results\varyWind-varyWhale\ev_table.csv", header=None
+).to_numpy()
+plot_data(range(len(table[100, :])), table[100, :], legend="Moored")
+plot_data(
+    range(len(table[100, :])),
+    table[201, :],
+    xlabel="Stage",
+    ylabel="Expected Value",
+    title="Flying vs Moored for Battery Capacity = 100%",
+    legend="Flying",
+)
 plt.show()

67785274b8b9671ad04a63484c0ea0619d51302c
Brian Epstein <bepstein8@gatech.edu>
2025-02-11 15:53:08 -0500

    remove uneccesary imports

--- SolarSimulator/Scripts/plot_all_results.py
+++ SolarSimulator/Scripts/plot_all_results.py
@@ -1,10 +1,6 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from BaseClasses.plotting_base import DataProcessor
 
-dire = r"Results\Analysis"
+dire = r"Results\New-Trans\1000"
 processor = DataProcessor(directory=dire)  # Use "." for the current directory
 processor.process_files()
 df = processor.get_results_df()

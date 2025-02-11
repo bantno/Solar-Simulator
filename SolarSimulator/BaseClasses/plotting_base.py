cff6cfe5b02547de8a0efca0c1aee3d9e4b8f3d0
Brian Epstein <bepstein8@gatech.edu>
2025-02-10 20:39:57 -0500

    adjust plotting

--- SolarSimulator/BaseClasses/plotting_base.py
+++ SolarSimulator/BaseClasses/plotting_base.py
@@ -356,8 +356,9 @@ class DataProcessor:
                         median_reward,
                         mean_failure_step,
                         median_failure_step,
+                        num_failures
                     ) = self.calculate_mean_rewards_and_failures(
-                        os.path.join(self.directory, filename)
+                        os.path.join(self.directory, filename),num_timesteps
                     )
 
                     # Store results in a dictionary
@@ -377,6 +378,7 @@ class DataProcessor:
                             "MedianFailureStep": median_failure_step,
                             "MeanReward": mean_reward,
                             "MedianReward": median_reward,
+                            "NumFailues": num_failures
                         }
                     )
                 else:
@@ -428,7 +430,7 @@ class DataProcessor:
 
         plt.savefig(save_dir + rf"\optimal_battery_plot.png")
 
-    def calculate_mean_rewards_and_failures(self, filepath):
+    def calculate_mean_rewards_and_failures(self, filepath,num_timesteps):
         """Calculate the mean reward and failure step for a given pickle file."""
         df = pd.read_pickle(filepath)
 
@@ -437,8 +439,9 @@ class DataProcessor:
             median_reward = df["Reward"].median()
             mean_failure_step = round(df["LastStep"].mean())
             median_failure_step = df["LastStep"].median()
+            num_failures = len(df[df["LastStep"] < num_timesteps-1])
             print(f"Number of runs in dataset {filepath}: {len(df)}")
-            return mean_reward, median_reward, mean_failure_step, median_failure_step
+            return mean_reward, median_reward, mean_failure_step, median_failure_step, num_failures
         else:
             print(f"Missing columns in {filepath}")
             return None, None
